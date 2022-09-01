3a2916d9a9fd69c10af8d345ce1b4b93290a1be9
kingaptos <kingaptos@periodic.foundation>
2022-09-01 10:54:56 -0700

    [www] Use next-js image rendering for background.

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -5,8 +5,11 @@ import { NavigationLinks } from "../components/Navigation";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-20 h-full w-full bg-[url('/sand.png')] bg-cover">
-      <div className="alchemy-shadow max-w-[400px] w-[90%] h-[300px] md:h-[400px]"><Image layout="fill" src='/alchemy.svg' /></div>
+    <div className="flex flex-col items-center justify-center gap-20 h-full w-full">
+      <Image layout="fill" src='/sand.png'/>
+      <div className="alchemy-shadow max-w-[400px] w-[90%] h-[300px] md:h-[400px]">
+        <Image layout="fill" src='/alchemy.svg' />
+      </div>
       <div className="flex flex-row gap-10">
         <a target="_blank" rel="noopener noreferrer" href={NavigationLinks.twitter}>
           <Image className="transition duration-300 hover:opacity-40 cursor-pointer" src="/twitter.svg" width={40} height={40} />
