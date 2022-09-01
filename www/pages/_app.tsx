4a9e9025258f977ef337c49b618bff4e25524d54
kingaptos <kingaptos@periodic.foundation>
2022-09-01 11:54:53 -0700

    [www] Test viewport-fit=cover.

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Script defer data-domain="ferum.xyz" src="https://plausible.io/js/plausible.js" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover"
         />
       </Head>
       <Component {...pageProps} />
