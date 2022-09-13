357cc3af755954a4d79100b2daaabeffd774d9e1
kingaptos <kingaptos@periodic.foundation>
2022-09-12 17:21:26 -0700

    [www] Add plausible provider.

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -3,20 +3,21 @@ import DEFAULT_APP_SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Script from 'next/script'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...DEFAULT_APP_SEO} />
       <Head>
-      <Script defer data-domain="ferum.xyz" src="https://plausible.io/js/plausible.js" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover"
         />
       </Head>
-      <Component {...pageProps} />
+      <PlausibleProvider domain="ferum.xyz">
+        <Component {...pageProps} />
+      </PlausibleProvider>
     </>
   );
 }
