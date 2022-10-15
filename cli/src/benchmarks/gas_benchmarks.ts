d440dbc1117e089f25632dcb4d2a1faf454076cd
xrelapse <xrelapse@hotmail.com>
2022-10-15 18:49:19 -0500

    Benchmarking script tbd

--- cli/src/benchmarks/gas_benchmarks.ts
+++ cli/src/benchmarks/gas_benchmarks.ts
@@ -2,15 +2,15 @@ import { AptosAccount } from 'aptos';
 import log from "loglevel";
 
 import { getClient, getFaucetClient, getNodeURL } from '../aptos-client';
-import { addOrder, initializeFerum, initializeMarket, addLimitTxnPayload } from '../market';
+import { addOrder, initializeFerum, initializeMarket, addOrderTxnPayload } from '../market';
 import { initializeUSDF, mintUSDF } from '../usdf';
-import { setEnv } from '../utils/env';
 import { publishModuleUsingCLI } from "../utils/module-publish-utils";
 import { simulateTransactionWithAccount } from '../utils/transaction-utils';
+import Config from '../config';
 
 log.setLevel("info");
 
-setEnv('devnet');
+Config.setEnv('devnet');
 
 const ORDER_COUNT = 100;
 
@@ -20,12 +20,12 @@ async function main() {
   log.info(`Creating account ${account.address().toString()} and funding with 1,000,000 APT`);
 
   let txHash = (await getFaucetClient().fundAccount(account.address(), 100000000000000))[0];
-  await getClient().waitForTransaction(txHash[0], {checkSuccess: true});
+  await getClient().waitForTransaction(txHash, {checkSuccess: true});
   log.info(`Account is funded`);
 
   // Delploy Ferum to that account.
   log.info('Publishing modules under account', account.address().toString());
-  await publishModuleUsingCLI(getNodeURL(), account, './contract', 10000000000000);
+  await publishModuleUsingCLI(Config.getEnv(), getNodeURL(), account, './contract', 10000000000000);
   log.info('Published');
 
   // Create and mint USDF coin.
@@ -47,50 +47,53 @@ async function main() {
   );
   await getClient().waitForTransaction(txHash, {checkSuccess: true});
 
-  // Submit ORDER_COUNT sell and buy limit orders.
-  const promises = [];
-  const midPrice = ORDER_COUNT * 10000;
-  for (let i = 0; i < ORDER_COUNT; i++) {
-    promises.push(new Promise<void>(async resolve => {
-      await addOrder(
-        account, 
-        '0x1::aptos_coin::AptosCoin',
-        `${account.address()}::test_coins::USDF`,
-        'sell',
-        midPrice + (i + 1),
-        1,
-      );
-      await getClient().waitForTransaction(txHash, {checkSuccess: true});
-      resolve();
-    }));
-  }
-  for (let i = 0; i < ORDER_COUNT; i++) {
-    promises.push(new Promise<void>(async resolve => {
-      await addOrder(
-        account, 
-        '0x1::aptos_coin::AptosCoin',
-        `${account.address()}::test_coins::USDF`,
-        'buy',
-        midPrice - (i + 1),
-        1,
-      );
-      await getClient().waitForTransaction(txHash, {checkSuccess: true});
-      resolve();
-    }));
-  }
-  await Promise.all(promises);
-  
-
-  // Simulate order that will eat the entire sell side of a book.
-  let payload = addLimitTxnPayload(
-    account, 
-    '0x1::aptos_coin::AptosCoin',
-    `${account.address()}::test_coins::USDF`,
-    'buy',
-    midPrice + 2*ORDER_COUNT,
-    1,
-  )
-  simulateTransactionWithAccount(account, payload);
+  // // Submit ORDER_COUNT sell and buy limit orders.
+  // const promises = [];
+  // const midPrice = ORDER_COUNT * 10000;
+  // for (let i = 0; i < ORDER_COUNT; i++) {
+  //   promises.push(new Promise<void>(async resolve => {
+  //     await addOrder(
+  //       account,
+  //       '0x1::aptos_coin::AptosCoin',
+  //       `${account.address()}::test_coins::USDF`,
+  //       'sell',
+  //       'resting',
+  //       midPrice + (i + 1),
+  //       1,
+  //     );
+  //     await getClient().waitForTransaction(txHash, {checkSuccess: true});
+  //     resolve();
+  //   }));
+  // }
+  // for (let i = 0; i < ORDER_COUNT; i++) {
+  //   promises.push(new Promise<void>(async resolve => {
+  //     await addOrder(
+  //       account,
+  //       '0x1::aptos_coin::AptosCoin',
+  //       `${account.address()}::test_coins::USDF`,
+  //       'buy',
+  //       'resting',
+  //       midPrice - (i + 1),
+  //       1,
+  //     );
+  //     await getClient().waitForTransaction(txHash, {checkSuccess: true});
+  //     resolve();
+  //   }));
+  // }
+  // await Promise.all(promises);
+  //
+  //
+  // // Simulate order that will eat the entire sell side of a book.
+  // let payload = addOrderTxnPayload(
+  //   account,
+  //   '0x1::aptos_coin::AptosCoin',
+  //   `${account.address()}::test_coins::USDF`,
+  //   'buy',
+  //   'resting',
+  //   midPrice + 2*ORDER_COUNT,
+  //   1,
+  // )
+  // simulateTransactionWithAccount(account, payload);
 }
 
 main();
